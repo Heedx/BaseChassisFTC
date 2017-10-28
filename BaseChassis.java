ed9cfd4412f468884a8e35c7f709e47b6ba15d74
Josh Brown <brownbear1002@gmail.com>
2017-10-28 14:56:02 -0400

    Seperated File Tree

--- BaseChassis.java
+++ BaseChassis.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * This file contains 13917's bass chassis test code for 2018 Relic Recovery.  It drive 
+ * This file contains 13917's bass chassis test code for 2018 Relic Recovery.  It drive
  * 2 motors tankdrive and 2 motors for intake.  Also has an arm servo for jewels.
  * Use this to test new features and teach.
  *
@@ -40,7 +40,7 @@ public class BaseChassis extends LinearOpMode {
         intakeRight = hardwareMap.get(DcMotor.class, "intakeRight");
         armServo = hardwareMap.get(Servo.class, "armServo");
         glyphSwitch = hardwareMap.get(DigitalChannel.class, "glyphSwitch");
-        
+
         double driveLeftPower = 0;
         double driveRightPower = 0;
         double drivePower = 0;
@@ -49,7 +49,7 @@ public class BaseChassis extends LinearOpMode {
         double ARM_EXTENDED = 0.0;
         double ARM_RETRACTED = 0.5;
         boolean intakeOn = false;
-        
+
         //have motors running in right direction
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
         intakeLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -57,19 +57,19 @@ public class BaseChassis extends LinearOpMode {
         armServo.setPosition(ARM_RETRACTED);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
-            
+
             drivePower = -this.gamepad1.left_stick_y;
             turnPower = -this.gamepad1.right_stick_x;
             //arcade drive
             motorLeft.setPower(drivePower - turnPower);
             motorRight.setPower(drivePower + turnPower);
-            
+
             if(gamepad1.left_bumper){
                 intakeOn = true;
             }
@@ -101,13 +101,12 @@ public class BaseChassis extends LinearOpMode {
             if(gamepad1.b){
                 armServo.setPosition(ARM_RETRACTED);
             }
-            
+
             //report values to drive station
             telemetry.addData("LeftPower", motorLeft.getPower());
             telemetry.addData("RightPower", motorRight.getPower());
             telemetry.addData("IntakeOn", intakeOn);
             telemetry.update();
-            
+
         }
     }
-
